851600466f8da4becc5b2fe131f878cb86a1660c
Huynh Thi Khanh Linh <bongudth@users.noreply.github.com>
2021-07-26 12:13:40 +0700

    :hammer: update scipte.js

--- script.js
+++ script.js
@@ -40,3 +40,63 @@ const quizData = [
     correct: 'd'
   }
 ]
+
+const question = document.getElementById('question')
+const answer_a = document.getElementById('answer_a')
+const answer_b = document.getElementById('answer_b')
+const answer_c = document.getElementById('answer_c')
+const answer_d = document.getElementById('answer_d')
+const answers = document.querySelectorAll('.answer')
+const quiz = document.getElementById('quiz')
+const submit = document.getElementById('submit')
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+  deslectAnswers();
+
+  const currentQuizData = quizData[currentQuiz];
+
+  question.innerText = currentQuizData.question;
+  answer_a.innerText = currentQuizData.a;
+  answer_b.innerText = currentQuizData.b;
+  answer_c.innerText = currentQuizData.c;
+  answer_d.innerText = currentQuizData.d;
+}
+
+function getSelected() {
+  let currentAnswer = undefined;
+
+  answers.forEach(answer => {
+    if (answer.checked) {
+      currentAnswer = answer.id;
+    }
+  })
+
+  return currentAnswer;
+}
+
+function deslectAnswers() {
+  answers.forEach(answer => {
+    answer.checked = false;
+  })
+}
+
+submit.addEventListener('click', () => {
+  const currentAnswer = getSelected();
+
+  if (currentAnswer) {
+    if (currentAnswer === quizData[currentQuiz].correct) {
+      score++;
+    }
+    currentQuiz++;
+    if (currentQuiz < quizData.length) {
+      loadQuiz();
+    } else {
+      quiz.innerHTML = `<h2>You answered correctly at ${score}/${quizData.length} questions.</h2><button onClick="location.reload()">Reload</button>`
+    }
+  }
+})
